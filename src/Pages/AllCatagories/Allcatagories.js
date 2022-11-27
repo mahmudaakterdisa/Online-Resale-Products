7133c7f03e2b171f8c41eab54eb578454a431daf
MahmudaDisa <dishamirza.h@gmail.com>
2022-11-27 21:01:33 +0100

    few changes in categories

--- src/Pages/AllCatagories/Allcatagories.js
+++ src/Pages/AllCatagories/Allcatagories.js
@@ -14,19 +14,17 @@ const Allcatagories = () => {
 
 
     const [furniture, setFurniture] = useState(null);
-    const { catagory_name, catagory_product } = getcatagory;
+    // const { catagory_name, catagory_product } = getcatagory;
 
 
 
 
     return (
         <div className='allcatagories-container'>
-            <div className='allcatagories-text'>
-                <h1 className='text-4xl'>{catagory_name}</h1>
-            </div>
+
             <div className='grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 allproducts-container'>
                 {
-                    catagory_product.map(products => <Availableproducts key={products.product_id} products={products} setFurniture={setFurniture}></Availableproducts>)
+                    getcatagory.map(products => <Availableproducts key={products.product_id} products={products} setFurniture={setFurniture}></Availableproducts>)
                 }
 
 
